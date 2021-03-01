ca3ca7fd38664e4a2b0e49b60441d1c23b5f0879
DiegoFernandoTrujilloAlvarez <diego.trujilloalvarez@gmail.com>
2021-03-01 15:05:19 -0500

    6-12 MockUps

---
+++ SportsLink/app/src/main/java/com/movil/sportslink/Encuentro_Usuarios.java
@@ -0,0 +1,14 @@
+package com.movil.sportslink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Encuentro_Usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encuentro__usuarios);
+    }
+}
---
+++ SportsLink/app/src/main/java/com/movil/sportslink/Recorrido_Automatico.java
@@ -0,0 +1,14 @@
+package com.movil.sportslink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recorrido_Automatico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorrido__automatico);
+    }
+}
