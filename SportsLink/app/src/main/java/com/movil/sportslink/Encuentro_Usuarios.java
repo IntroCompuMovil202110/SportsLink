139bb9f616e06ef5832c03ddd4b1b8207a3ab773
DiegoFernandoTrujilloAlvarez <diego.trujilloalvarez@gmail.com>
2021-03-02 08:30:01 -0500

    funcionalidad de los botones de los mockups 6-12

--- SportsLink/app/src/main/java/com/movil/sportslink/Encuentro_Usuarios.java
+++ SportsLink/app/src/main/java/com/movil/sportslink/Encuentro_Usuarios.java
@@ -2,13 +2,33 @@ package com.movil.sportslink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Encuentro_Usuarios extends AppCompatActivity {
-
+    Button participante3;
+    Button participante4;
+    Button ruta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encuentro__usuarios);
+        participante3 = findViewById(R.id.buttonParticipante3);
+        participante4 = findViewById(R.id.buttonParticipante4);
+        ruta = findViewById(R.id.buttonRuta);
+
+        participante3.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), Perfil_Vista.class);
+            startActivity(intent);
+        });
+        participante4.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), Perfil_Vista.class);
+            startActivity(intent);
+        });
+        ruta.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), EnrutamientoEncuentro.class);
+            startActivity(intent);
+        });
     }
 }
--- SportsLink/app/src/main/java/com/movil/sportslink/Recorrido_Manual.java
+++ SportsLink/app/src/main/java/com/movil/sportslink/Recorrido_Manual.java
@@ -3,12 +3,15 @@ package com.movil.sportslink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Recorrido_Manual extends AppCompatActivity {
-
+    Button crearRuta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recorrido__manual);
+        crearRuta = findViewById(R.id.buttonCrearRuta);
+        crearRuta.setOnClickListener(v -> finish());
     }
 }
