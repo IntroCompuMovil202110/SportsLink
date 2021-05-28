206b81f8e92abe77c5a6b44b9048a475c087c4cb
SantiagoCaroprese <66383253+SantiagoCaroprese@users.noreply.github.com>
2021-05-28 14:31:56 -0500

    LoginFuncionando

---
+++ Flutter/sportslink/android/app/src/main/kotlin/com/example/sportslink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sportslink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/sportslink/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'PaginaInicio.dart';
+import 'AuthenticationService.dart';
+import 'SignIn.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+
